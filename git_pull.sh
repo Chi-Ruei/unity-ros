ecbcaa727a3648c6a2bd39dbf6f075cd6818e6f6
Chi-Ruei <rickw@gmail.com>
2021-06-03 14:52:41 +0800

    test

--- git_pull.sh
+++ git_pull.sh
@@ -18,5 +18,18 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    return 1
 fi
 
+echo "-----------------------------------------------------------------------"
+echo "-------------------------pull VR-locobot--------------------------------"
+echo "-----------------------------------------------------------------------"
+cd ~/unity-ros/ROS/catkin_ws/src/VR-locobot
+git checkout $BRANCH
+git pull
+
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+   echo "There is conflict in oculusVR. Aborting"
+   return 1
+fi
+
 cd ~/unity-ros
 return 0
